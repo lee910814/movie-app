60053fd875356236e041bdea6b7dd3a6df8b6117
lee910814 <wantto13>
2021-11-21 23:25:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies :[]
+  };
+
+  getMovies = async() => {
+    const movies = await axios.get("http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=ff0a4d986db525f6d3fa4581401e1218&targetDt=20210101")
+  }
+  componentDidCatch(){
+   this.getMovies();
+
+  }
+  render(){
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "haha"}</div>
+  }
+
 }
-
 export default App;
