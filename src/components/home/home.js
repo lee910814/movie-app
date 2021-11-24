013a4e4f22d2278eaa8cb5f5e5e143030566255d
lee910814 <wantto13>
2021-11-24 21:25:54 +0900

    add home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Nav from "../nav/Nav";
+import Content from "./content/content";
+import Image from "./image/image";
 import styles from './home.module.css';
 
 function Home(){
     return(
         <div className={styles.home}>
          <Nav/>
+         <div className={styles.main}>
+             <Content/>
+             <Image/>
+         </div>
         </div>
     )
 }
